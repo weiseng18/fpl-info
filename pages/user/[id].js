2ae83128bddf54f3a652fd7e080a55b18d2eea3d
weiseng18 <tws.hci@gmail.com>
2021-05-26 20:34:26 +0800

    feat: create page to trigger user endpoint

---
+++ pages/user/[id].js
@@ -0,0 +1,48 @@
+import { VStack, Table, Thead, Tbody, Tr, Th, Td, TableCaption } from "@chakra-ui/react"
+
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+const User = () => {
+  const router = useRouter()
+  const { id } = router.query
+
+  const [seasonsData, setSeasonsData] = useState([])
+
+  useEffect(() => {
+    const getUserHistory = async (id) => {
+      const res = await axios.get(`/api/user/${id}`)
+      setSeasonsData(res.data)
+    }
+    // if ID param is provided
+    if (id)
+      getUserHistory(id)
+  }, [id])
+
+  return (
+    <VStack my="10" justifyContent="center">
+      <Table variant="striped" w="70%">
+        <TableCaption>FPL overall performance for user {id}</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>Season</Th>
+            <Th>Total Points</Th>
+            <Th>Overall Rank</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {seasonsData.map((one) => (
+            <Tr>
+              <Td>{one.season_name}</Td>
+              <Td>{one.total_points}</Td>
+              <Td>{one.rank}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </VStack>
+  )
+}
+
+export default User
