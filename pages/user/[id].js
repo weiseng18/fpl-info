b22cc143925264f918893a1c305b36e4f45239fd
weiseng18 <tws.hci@gmail.com>
2021-05-30 16:57:17 +0800

    feat: add multiple API calls to frontend to get more data
    - GET /api/players/names
    - GET /api/gameweeks/:gameweekId
    - GET /api/users/:userId/gameweeks/:gameweekId

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -9,15 +9,53 @@ const User = () => {
   const { id } = router.query
 
   const [seasonsData, setSeasonsData] = useState([])
+  const [playerNames, setPlayerNames] = useState([])
+  const [playerPoints, setPlayerPoints] = useState([])
+  const [picks, setPicks] = useState([])
 
   useEffect(() => {
+    /**
+     * Helper functions
+     */
     const getUserHistory = async (id) => {
       const res = await axios.get(`/api/users/${id}`)
       setSeasonsData(res.data)
     }
+    const getPlayerNames = async () => {
+      const res = await axios.get('/api/players/names')
+      setPlayerNames(res.data)
+    }
+    const getGameweekPoints = async () => {
+      let allGameweekPoints = []
+      for (let i=1; i<=38; i++) {
+        const res = await axios.get(`/api/gameweeks/${i}`)
+        const points = res.data
+        allGameweekPoints.push(points)
+      }
+      setPlayerPoints(allGameweekPoints)
+    }
+    const getGameweekPicks = async (id) => {
+      let allGameweekPicks = []
+      for (let i=1; i<=38; i++) {
+        const res = await axios.get(`/api/users/${id}/gameweeks/${i}`)
+        const picks = res.data
+        allGameweekPicks.push(picks)
+      }
+      setPicks(allGameweekPicks)
+    }
+
+    /**
+     * Get data
+     */
+    getPlayerNames()
+    getGameweekPoints()
+
     // if ID param is provided
-    if (id)
+    if (id) {
       getUserHistory(id)
+      getGameweekPicks(id)
+    }
+
   }, [id])
 
   return (
