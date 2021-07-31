bee76bb76da28d951eb0584103f11970cb848ee8
weiseng18 <tws.hci@gmail.com>
2021-07-31 12:54:54 +0800

    chore: lint

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
-import { Container, Flex, VStack, Heading, Text, InputGroup, InputLeftAddon, Input, Button } from "@chakra-ui/react"
+import {
+  Container,
+  Flex,
+  VStack,
+  Heading,
+  Text,
+  InputGroup,
+  InputLeftAddon,
+  Input,
+  Button,
+} from "@chakra-ui/react"
 
 import Router from "next/router"
 
 import { useState } from "react"
 
 const Index = () => {
-
   const [ID, setID] = useState("")
   const [invalid, setInvalid] = useState(false)
   const [error, setError] = useState("")
@@ -23,8 +32,7 @@ const Index = () => {
   }
 
   const handleKeydown = (e) => {
-    if (e.keyCode == 13 || e.key == "Enter")
-      handleSubmit()
+    if (e.keyCode == 13 || e.key == "Enter") handleSubmit()
   }
 
   const handleSubmit = () => {
@@ -42,32 +50,35 @@ const Index = () => {
   }
 
   return (
-      <Container h="100vh">
-        <Flex h="100%" justifyContent="center" alignItems="center">
-          <VStack spacing="60px">
-            <Heading>Learn more about your FPL team</Heading>
-            <VStack spacing={8}>
-              <InputGroup>
-                <InputLeftAddon children={<Text fontWeight="600">FPL ID</Text>} />
-                <Input
-                  isRequired
-                  isInvalid={invalid}
-                  errorBorderColor="crimson"
-                  onChange={handleChange}
-                  onKeyDown={handleKeydown}
-                  placeholder="Fill in your FPL ID"
-                />
-              </InputGroup>
-              {invalid && error && (
-                <Text color="crimson">Error: {error}</Text>
-              )}
-              <Button disabled={invalid || !ID.length} onClick={handleSubmit}>Submit</Button>
-            </VStack>
-            {OFF_SEASON && (
-              <Text>It is currently off season for FPL. Only overall season data will be provided.</Text>
-            )}
+    <Container h="100vh">
+      <Flex h="100%" justifyContent="center" alignItems="center">
+        <VStack spacing="60px">
+          <Heading>Learn more about your FPL team</Heading>
+          <VStack spacing={8}>
+            <InputGroup>
+              <InputLeftAddon children={<Text fontWeight="600">FPL ID</Text>} />
+              <Input
+                isRequired
+                isInvalid={invalid}
+                errorBorderColor="crimson"
+                onChange={handleChange}
+                onKeyDown={handleKeydown}
+                placeholder="Fill in your FPL ID"
+              />
+            </InputGroup>
+            {invalid && error && <Text color="crimson">Error: {error}</Text>}
+            <Button disabled={invalid || !ID.length} onClick={handleSubmit}>
+              Submit
+            </Button>
           </VStack>
-        </Flex>
+          {OFF_SEASON && (
+            <Text>
+              It is currently off season for FPL. Only overall season data will
+              be provided.
+            </Text>
+          )}
+        </VStack>
+      </Flex>
     </Container>
   )
 }
